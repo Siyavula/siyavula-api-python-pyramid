269bc57c84a47017f24a0217b149fccecfec4329
Lehan Coetzee <lehan@siyavula.com>
2020-10-08 13:49:09 +0200

    Add practice activity

--- siyavula_api_python_pyramid/siyavula_api_python_pyramid/routes.py
+++ siyavula_api_python_pyramid/siyavula_api_python_pyramid/routes.py
@@ -2,3 +2,4 @@ def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('standalone_responsive', '/standalone')
     config.add_route('assignment_responsive', '/assignment')
+    config.add_route('practice_responsive', '/practice')
