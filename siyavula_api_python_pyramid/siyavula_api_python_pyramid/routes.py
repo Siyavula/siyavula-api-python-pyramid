312fa2f65d2c099a13579fa8d573edbbe913593a
Lehan <haaslewer2@gmail.com>
2022-06-13 13:54:20 +0200

    Add standalone list activity

--- siyavula_api_python_pyramid/siyavula_api_python_pyramid/routes.py
+++ siyavula_api_python_pyramid/siyavula_api_python_pyramid/routes.py
@@ -1,6 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('standalone_responsive', '/standalone')
+    config.add_route('standalone_list_responsive', '/standalone-list')
     config.add_route('assignment_responsive', '/assignment')
     config.add_route('practice_responsive', '/practice')
     config.add_route('practice_toc', '/practice-toc')
